a3d9c2117ce6230bbb0e22274f5d3a0673ac0991
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-01-05 18:13:31 +0100

    feat: generate nrwl-airlines Angular app projects

---
+++ e2e/examples/angular/nrwl-airlines/booking/desktop-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nrwl-airlines-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'examples-angular-nrwl-airlines-booking-desktop-app';
+}
--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
     '<rootDir>/e2e/angular-e2e',
     '<rootDir>/packages/angular',
     '<rootDir>/tools',
+    '<rootDir>/e2e/examples/angular/nrwl-airlines/booking/desktop-app',
+    '<rootDir>/e2e/examples/angular/nrwl-airlines/booking/mobile-app',
+    '<rootDir>/e2e/examples/angular/nrwl-airlines/check-in/desktop-app',
+    '<rootDir>/e2e/examples/angular/nrwl-airlines/check-in/mobile-app',
   ],
 };
