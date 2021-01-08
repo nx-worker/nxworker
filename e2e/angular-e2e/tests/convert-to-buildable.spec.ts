08b129f58bcf3c3f818ad7a396c4ed7ecf5947ca
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-01-08 02:22:48 +0100

    test(angular): cover adding ng-packagr

--- e2e/angular-e2e/tests/convert-to-buildable.spec.ts
+++ e2e/angular-e2e/tests/convert-to-buildable.spec.ts
@@ -1,4 +1,11 @@
-import { checkFilesExist, copyNodeModules, ensureNxProject, runNxCommandAsync, uniq } from '@nrwl/nx-plugin/testing';
+import {
+  checkFilesExist,
+  copyNodeModules,
+  ensureNxProject,
+  readJson,
+  runNxCommandAsync,
+  uniq,
+} from '@nrwl/nx-plugin/testing';
 import * as path from 'path';
 
 describe('@nxworker/angular:convert-to-buildable generator e2e', () => {
@@ -6,21 +13,36 @@ describe('@nxworker/angular:convert-to-buildable generator e2e', () => {
     copyNodeModules(['@nrwl/angular']);
   });
 
+  beforeEach(async () => {
+    ensureNxProject('@nxworker/angular', 'dist/packages/angular');
+    projectName = uniq('test-library');
+    await runNxCommandAsync(`generate @nrwl/angular:library ${projectName}`);
+  }, 30000);
+
+  let projectName: string;
+
   it('generates buildable library configurations', async done => {
-    const projectName = uniq('test-library');
     const configurationFileNames = [
       'ng-package.json',
       'package.json',
       'tsconfig.lib.prod.json',
     ].map(fileName => path.join('libs', projectName, fileName));
-    ensureNxProject('@nxworker/angular', 'dist/packages/angular');
-    await runNxCommandAsync(`generate @nrwl/angular:library ${projectName}`);
 
     await runNxCommandAsync(
       `generate @nxworker/angular:convert-to-buildable ${projectName}`
     );
 
     expect(() => checkFilesExist(...configurationFileNames)).not.toThrow();
+    done();
+  }, 30000);
+
+  it('adds ng-packagr', async done => {
+    await runNxCommandAsync(
+      `generate @nxworker/angular:convert-to-buildable ${projectName}`
+    );
+
+    const { devDependencies = {} } = readJson('package.json');
+    expect(devDependencies['ng-packagr']).toBeDefined();
     done();
   }, 30000);
 });
