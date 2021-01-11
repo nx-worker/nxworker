ab871c2fb9e017705b005b0e4b9d356fbf9cd56b
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-01-11 22:06:48 +0100

    test(angular): use Yarn for end-to-end tests

--- e2e/angular-e2e/tests/convert-to-buildable.spec.ts
+++ e2e/angular-e2e/tests/convert-to-buildable.spec.ts
@@ -1,3 +1,4 @@
+import { WorkspaceConfiguration as NxWorkspaceConfiguration } from '@nrwl/devkit';
 import {
   checkFilesExist,
   copyNodeModules,
@@ -5,18 +6,36 @@ import {
   readJson,
   runNxCommand,
   runNxCommandAsync,
-  runPackageManagerInstall,
   uniq,
+  updateFile,
 } from '@nrwl/nx-plugin/testing';
 import * as path from 'path';
 
+interface WorkspaceConfiguration extends NxWorkspaceConfiguration {
+  cli?: {
+    defaultCollection: string;
+    packageManager: 'npm' | 'pnpm' | 'yarn';
+  };
+}
+
 describe('@nxworker/angular:convert-to-buildable generator e2e', () => {
   beforeAll(() => {
-    copyNodeModules(['@nrwl/angular', 'ng-packagr']);
-    runPackageManagerInstall();
+    copyNodeModules(['ng-packagr']);
   });
 
   beforeEach(async () => {
+    updateFile('workspace.json', raw => {
+      const workspaceJson: WorkspaceConfiguration = JSON.parse(raw);
+      const workspaceJsonUsingYarn: WorkspaceConfiguration = {
+        ...workspaceJson,
+        cli: {
+          ...(workspaceJson.cli ?? {}),
+          packageManager: 'yarn',
+        },
+      };
+
+      return JSON.stringify(workspaceJsonUsingYarn, null, 2);
+    });
     ensureNxProject('@nxworker/angular', 'dist/packages/angular');
     projectName = uniq('test-library');
     await runNxCommandAsync(`generate @nrwl/angular:library ${projectName}`);
