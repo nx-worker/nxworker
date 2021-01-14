dcdf183e04268c66b86a4c8841fc64e2e6e8c603
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-01-14 01:08:56 +0100

    test(angular): fix pnpm install issue in CI

--- packages/internal/e2e-util/src/lib/fix-pnpm-install-in-ci-pipeline.ts
+++ packages/internal/e2e-util/src/lib/fix-pnpm-install-in-ci-pipeline.ts
@@ -1,15 +1,9 @@
-import { updateFile } from '@nrwl/nx-plugin/testing';
-
-function appendLine(extraLine: string, content: string): string {
-  return [content, extraLine].filter(line => line !== '').join('\n');
-}
+import { runCommand } from '@nrwl/nx-plugin/testing';
 
 /**
  * Fixes a `pnpm install` issue in CI pipelines because the `--frozen-lockfile`
  * parameter is enabled when the environment has `CI=true`.
  */
 export function fixPnpmInstallInCiPipeline() {
-  updateFile('.npmrc', content =>
-    appendLine('prefer-frozen-lockfile=false', content)
-  );
+  runCommand('"prefer-frozen-lockfile=false" >> .npmrc');
 }
