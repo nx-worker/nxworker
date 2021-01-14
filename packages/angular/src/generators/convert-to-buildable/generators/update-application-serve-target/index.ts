f95500fff7e9d18276002bb02edc1576505e896c
Nacho Vazquez <nachovc1410@gmail.com>
2021-01-14 20:14:02 -0300

    feat: 🎸 add updateApplicationServeTarget generator

---
+++ packages/angular/src/generators/convert-to-buildable/generators/update-application-serve-target/index.ts
@@ -0,0 +1,1 @@
+export * from './update-application-serve-target';
---
+++ packages/angular/src/generators/convert-to-buildable/generators/update-application-serve-target/update-application-serve-target.ts
@@ -0,0 +1,72 @@
+import {
+  TargetConfiguration,
+  Tree,
+  updateProjectConfiguration,
+} from '@nrwl/devkit';
+
+import { NormalizedSchema } from '../../util';
+
+const defaultAngularServeExecutor = '@angular-devkit/build-angular:dev-server';
+const incrementalServeExecutor = '@nrwl/web:file-server';
+
+export function updateApplicationServeTarget(
+  host: Tree,
+  { projectConfiguration, projectName, projectType }: NormalizedSchema
+): void {
+  if (projectType !== 'application') {
+    console.error(
+      `Project with name "${projectName}" is not an application. Skipping..."`
+    );
+
+    return;
+  }
+
+  const { targets: executionTargets } = projectConfiguration;
+  const { serve: serveTarget } = executionTargets;
+
+  if (!serveTarget) {
+    console.warn(
+      `Application with project name "${projectName}" does not has an existing "serve" target. Skipping..."`
+    );
+
+    return;
+  }
+
+  const { executor } = serveTarget;
+
+  if (executor === incrementalServeExecutor) {
+    console.info(
+      `Application with name "${projectName}" is already set up for incremental build. Skipping...`
+    );
+
+    return;
+  }
+
+  if (executor !== defaultAngularServeExecutor) {
+    console.error(
+      `Unexpected builder/executor "${executor}" for "serve" target of project with name "${projectName}". Skipping..."`
+    );
+
+    return;
+  }
+
+  const incrementalBuildTarget: TargetConfiguration = {
+    ...serveTarget,
+    executor: incrementalServeExecutor,
+    options: {
+      ...(serveTarget.options ?? {}),
+      // I would default these, but I would add a `skipExecutorOptions`
+      //  optional flag as a generator argument.
+      parallel: true,
+      withDeps: true,
+    },
+  };
+
+  updateProjectConfiguration(host, projectName, {
+    ...projectConfiguration,
+    targets: {
+      ...executionTargets,
+      serve: incrementalBuildTarget,
+    },
+  });
+}
