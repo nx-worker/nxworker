dc55a79b825296311e0cdd5d9cca8bfe5b0f2c6c
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-01-17 13:28:26 +0100

    test(angular): refactor addNgPackagr unit test

--- packages/angular/src/generators/convert-to-buildable/generators/add-ng-packagr/add-ng-packagr.spec.ts
+++ packages/angular/src/generators/convert-to-buildable/generators/add-ng-packagr/add-ng-packagr.spec.ts
@@ -1,62 +1,25 @@
-import {
-  addProjectConfiguration,
-  ProjectConfiguration,
-  readJson,
-  Tree,
-} from '@nrwl/devkit';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { addAngularLibrary, LibraryType } from '@internal/test-util';
+import { readJson, Tree } from '@nrwl/devkit';
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 
-import {
-  TsconfigBaseJson,
-  WorkspaceRootPackageJson,
-} from '../../../../file-types';
+import { WorkspaceRootPackageJson } from '../../../../file-types';
 import { addNgPackagr } from './add-ng-packagr';
 
 describe(addNgPackagr.name, () => {
   beforeEach(() => {
     host = createTreeWithEmptyWorkspace();
-    projectName = 'shared-ui-buttons';
-    importPath = `@nrwl-airlines/shared/ui-buttons`;
-    project = {
-      projectType: 'library',
-      root: 'libs/shared/ui-buttons',
-      sourceRoot: 'libs/shared/ui-buttons/src',
-      targets: {
-        lint: {
-          executor: '@nrwl/linter:eslint',
-          options: {
-            lintFilePatterns: [
-              'libs/shared/ui-buttons/src/**/*.ts',
-              'libs/shared/ui-buttons/src/**/*.html',
-            ],
-          },
-        },
-        test: {
-          executor: '@nrwl/jest:jest',
-          outputs: ['coverage/libs/shared/ui-buttons'],
-          options: {
-            jestConfig: 'libs/shared/ui-buttons/jest.config.js',
-            passWithNoTests: true,
-          },
-        },
-      },
-    };
-    const tsconfigBase: TsconfigBaseJson = {
-      compilerOptions: {
-        paths: {
-          [importPath]: [`${project.sourceRoot}/index.ts`],
-        },
-      },
-    };
-
-    host.write('tsconfig.base.json', JSON.stringify(tsconfigBase));
-    addProjectConfiguration(host, projectName, project);
+    const name = 'ui-buttons';
+    const directory = 'shared';
+    addAngularLibrary(host, {
+      directory,
+      name,
+      npmScope: 'nrwl-airlines',
+      type: LibraryType.WorkspaceLibrary,
+    });
   });
 
   let host: Tree;
-  let importPath: string;
-  let project: ProjectConfiguration;
-  let projectName: string;
 
   it('installs ng-packagr when not installed', async () => {
     await addNgPackagr(host);
