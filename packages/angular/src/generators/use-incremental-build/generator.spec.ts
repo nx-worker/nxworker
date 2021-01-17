3f9af47976bf9f83f4b50d22ad8f16195f0a6467
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-01-17 13:54:02 +0100

    test(angular): refactor convert-to-buildable integration test, extract addAngularApplication utility

--- packages/angular/src/generators/use-incremental-build/generator.spec.ts
+++ packages/angular/src/generators/use-incremental-build/generator.spec.ts
@@ -1,9 +1,6 @@
-import {
-  addProjectConfiguration,
-  ProjectConfiguration,
-  readProjectConfiguration,
-  Tree,
-} from '@nrwl/devkit';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { addAngularApplication, createProjectName } from '@internal/test-util';
+import { readProjectConfiguration, Tree } from '@nrwl/devkit';
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 
 import generator from './generator';
@@ -11,102 +8,18 @@ import generator from './generator';
 describe('convert-to-buildable generator', () => {
   beforeEach(() => {
     host = createTreeWithEmptyWorkspace();
-    projectName = 'booking-desktop-app';
-    project = {
-      projectType: 'application',
-      root: 'apps/booking/desktop-app',
-      sourceRoot: 'apps/booking/desktop-app/src',
-      // prefix: 'booking',
-      targets: {
-        build: {
-          executor: '@angular-devkit/build-angular:browser',
-          options: {
-            outputPath: 'dist/apps/booking/desktop-app',
-            index: 'apps/booking/desktop-app/src/index.html',
-            main: 'apps/booking/desktop-app/src/main.ts',
-            polyfills: 'apps/booking/desktop-app/src/polyfills.ts',
-            tsConfig: 'apps/booking/desktop-app/tsconfig.app.json',
-            aot: true,
-            assets: [
-              'apps/booking/desktop-app/src/favicon.ico',
-              'apps/booking/desktop-app/src/assets',
-            ],
-            styles: ['apps/booking/desktop-app/src/styles.scss'],
-            scripts: [],
-          },
-          configurations: {
-            production: {
-              fileReplacements: [
-                {
-                  replace:
-                    'apps/booking/desktop-app/src/environments/environment.ts',
-                  with:
-                    'apps/booking/desktop-app/src/environments/environment.prod.ts',
-                },
-              ],
-              optimization: true,
-              outputHashing: 'all',
-              sourceMap: false,
-              namedChunks: false,
-              extractLicenses: true,
-              vendorChunk: false,
-              buildOptimizer: true,
-              budgets: [
-                {
-                  type: 'initial',
-                  maximumWarning: '2mb',
-                  maximumError: '5mb',
-                },
-                {
-                  type: 'anyComponentStyle',
-                  maximumWarning: '6kb',
-                  maximumError: '10kb',
-                },
-              ],
-            },
-          },
-        },
-        serve: {
-          executor: '@angular-devkit/build-angular:dev-server',
-          options: {
-            browserTarget: 'booking-desktop-app:build',
-          },
-          configurations: {
-            production: {
-              browserTarget: 'booking-desktop-app:build:production',
-            },
-          },
-        },
-        'extract-i18n': {
-          executor: '@angular-devkit/build-angular:extract-i18n',
-          options: {
-            browserTarget: 'booking-desktop-app:build',
-          },
-        },
-        lint: {
-          executor: '@nrwl/linter:eslint',
-          options: {
-            lintFilePatterns: [
-              'apps/booking/desktop-app/src/**/*.ts',
-              'apps/booking/desktop-app/src/**/*.html',
-            ],
-          },
-        },
-        test: {
-          executor: '@nrwl/jest:jest',
-          outputs: ['coverage/apps/booking/desktop-app'],
-          options: {
-            jestConfig: 'apps/booking/desktop-app/jest.config.js',
-            passWithNoTests: true,
-          },
-        },
-      },
-    };
-
-    addProjectConfiguration(host, projectName, project);
+    const name = 'desktop-app';
+    const directory = 'booking';
+    projectName = createProjectName({
+      directory,
+      name,
+    });
+    addAngularApplication(host, {
+      directory,
+      name,
+    });
   });
 
-  let project: ProjectConfiguration;
   let projectName: string;
   let host: Tree;
 
--- packages/internal/test-util/src/index.ts
+++ packages/internal/test-util/src/index.ts
@@ -1,2 +1,3 @@
+export * from './lib/generators/add-angular-application';
 export * from './lib/generators/add-angular-library';
 export * from './lib/util/create-project-name';
